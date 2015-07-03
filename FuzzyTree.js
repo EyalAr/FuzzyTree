ca81300b58a7db4acf4ed2e43ad06451df32663a
Eyal Arubas <eyalarubas@gmail.com>
2015-07-03 09:48:08 +0800

    wip

--- FuzzyTree.js
+++ FuzzyTree.js
@@ -1,22 +1,56 @@
+/* jshint esnext:true */
+
 import _ from "lodash";
 
-class FuzzyTree(){
+class FuzzyTree{
+
+    /**
+     * Construct a new FuzzyTree node.
+     */
     constructor(){
         this._separator = '.';
         this._wildcard = '*';
         this._greedy = '#';
-
+        // reset data, children and dummy:
         this.reset();
     }
 
-    setData(data){
-        this._data = data;
+    /**
+     * Remove this node's data and children.
+     * @return {FuzzyTree} The node itself.
+     */
+    reset(){
+        this._data = null;
+        this._children = {};
+        this._dummy = false;
+        return this;
     }
 
+    /**
+     * Set this node's data.
+     * @param {*} data The data to set.
+     * @return {FuzzyTree} The node itself.
+     */
+    setData(data){
+        this._data = data;
+        return this;
+    }
+
+    /**
+     * Get this node's data.
+     * @return {*} This node's data.
+     */
     getData(){
         return this._data;
     }
 
+    /**
+     * Gather all the nodes in this tree who's path pattern matches the given
+     * path.
+     * @param  {String|Array} path The path to match against.
+     * @return {Array} An array of FuzzyTree nodes who's path pattern matches
+     * the given path.
+     */
     match(path){
         path = path || [];
         if (_.isString(path)) path = path.split(this._separator);
@@ -29,22 +63,46 @@ class FuzzyTree(){
         if (path.some(s => s === this._wildcard || s === this._greedy))
             throw Error("path section cannot be a wildcard");
 
+        return this._match(path);
+    }
+
+    _match(path){
         var res = [], child, i;
-        if (!path.length) res.push(this);
-        else {
-            if (child = this._children[path[0]])
-                _push(res, child.find(path.slice(1)));
-            if (child = this._children[this._wildcard])
-                _push(res, child.find(path.slice(1)));
-            if (child = this._children[this._greedy])
+
+        if (!path.length && !this._dummy) {
+            // if the path is empty, return the node itself.
+            res.push(this);
+        } else {
+            // if the path to one of the children is equal to the next section
+            // in the requested path, traverse this child.
+            child = this._children[path[0]];
+            if (child)
+                _push(res, child.match(path.slice(1)));
+
+            // if one of the children's path pattern is a wildcard, it also
+            // matches the next section of the requested path. traverse it too.
+            child = this._children[this._wildcard];
+            if (child)
+                _push(res, child.match(path.slice(1)));
+
+            // if one of the children's path pattern is a greedy wildcard,
+            // traverse this child with all possible sub-paths of the requested
+            // path.
+            child = this._children[this._greedy];
+            if (child)
                 for(i = 1 ; i <= path.length ; i++){
-                    _push(res, child.find(path.slice(i)));
+                    _push(res, child.match(path.slice(i)));
                 }
         }
 
         return res;
     }
 
+    /**
+     * Find the node under a specific path pattern.
+     * @param  {String|Array} path The path pattern of the required node.
+     * @return {FuzzyTree|Null} The found node, or null if not found.
+     */
     find(path){
         path = path || [];
         if (_.isString(path)) path = path.split(this._separator);
@@ -55,12 +113,23 @@ class FuzzyTree(){
         if (path.some(s => s.length === 0))
             throw Error("path section cannot be empty");
 
-        if (!path.length) return this;
-        if (!this._children[path[0]]) return null;
-
-        return this._children[path[0].insert(path.slice(1));
+        return this._find(path);
     }
 
+    _find(path){
+        if (!path.length) return this._dummy ? null : this;
+        if (!this._children[path[0]]) return null;
+
+        return this._children[path[0]].find(path.slice(1));
+    }
+
+    /**
+     * Insert a node under the specified path pattern. New nodes will be created
+     * along the way if needed. If a node already exists under this path pattern
+     * it will be resetted.
+     * @param  {String|Array} path The path pattern of the new node.
+     * @return {FuzzyTree} The newly created node.
+     */
     insert(path){
         path = path || [];
         if (_.isString(path)) path = path.split(this._separator);
@@ -71,18 +140,19 @@ class FuzzyTree(){
         if (path.some(s => s.length === 0))
             throw Error("path section cannot be empty");
 
-        if (!path.length) return this.reset();
-
-        if (!this._children[path[0]])
-            this._children[path[0]] = new FuzzyTree();
-
-        return this._children[path[0].insert(path.slice(1));
+        return this._insert(path);
     }
 
-    reset(){
-        this._data = null;
-        this._children = {};
-        return this;
+    _insert(path){
+        if (!path.length) return this.reset();
+
+        if (!this._children[path[0]]){
+            // create a dummy node along the path
+            this._children[path[0]] = new FuzzyTree();
+            this._children[path[0]]._dummy = true;
+        }
+
+        return this._children[path[0]].insert(path.slice(1));
     }
 }
 
